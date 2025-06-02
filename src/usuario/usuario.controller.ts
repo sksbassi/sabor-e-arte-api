6bdc4cfb2000570c5330cebf3e45446dec3163ad
Joao Pires <joaovictor.2005.pires@gmail.com>
2025-06-01 21:56:57 -0300

    ajuste de rotas

--- src/usuario/usuario.controller.ts
+++ src/usuario/usuario.controller.ts
@@ -3,7 +3,7 @@ import { CreateUsuarioDto } from './dto/create-usuario.dto';
 import { UpdateUsuarioDto } from './dto/update-usuario.dto';
 import { UsuarioService } from './usuario.service';
 
-@Controller('usuario')
+@Controller('rotausuario')
 export class UsuarioController {
   constructor(private readonly usuarioService: UsuarioService) {}
 
