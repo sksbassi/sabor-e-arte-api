1c8e2c73c4a1fe1bb05729989e3832b568229f27
Joao Pires <joaovictor.2005.pires@gmail.com>
2025-06-02 21:56:08 -0300

    Ajuste de validação de email

--- src/usuario/usuario.controller.ts
+++ src/usuario/usuario.controller.ts
@@ -3,7 +3,7 @@ import { CreateUsuarioDto } from './dto/create-usuario.dto';
 import { UpdateUsuarioDto } from './dto/update-usuario.dto';
 import { UsuarioService } from './usuario.service';
 
-@Controller('rotausuario')
+@Controller('usuario')
 export class UsuarioController {
   constructor(private readonly usuarioService: UsuarioService) {}
 
